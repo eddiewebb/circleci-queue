76203f8e3327793bb03f831d65497c0ed2e2b298
EDWARD WEBB <EDWARD.WEBB@LibertyMutual.com>
2018-04-12 09:22:49 -0400

    Basic loop/wait logic implemented, no real api calls

--- src/circleci.py
+++ src/circleci.py
@@ -8,3 +8,11 @@ class CircleCI:
 
 	def __init__(self,circleci_key,build_num,user_name,project_name,repo_url):
 		self.user_name = user_name
+		self
+
+
+	def oldest_running_build_num(self):
+		return 1
+
+	def cancel_current(self):
+		return
--- src/queueBuildUntilFrontOfLine.py
+++ src/queueBuildUntilFrontOfLine.py
@@ -1,14 +1,44 @@
+import sys
+import time
 import circleci
 import environment
 
 
 circleci_key = environment.loadOrExit('CIRCLECI_API_KEY')
-build_num = environment.loadOrExit('CIRCLE_BUILD_NUM')
+build_num = int(environment.loadOrExit('CIRCLE_BUILD_NUM'))
 user_name = environment.loadOrExit('CIRCLE_PROJECT_USERNAME')
 project_name = environment.loadOrExit('CIRCLE_PROJECT_REPONAME')
 repo_url = environment.loadOrExit('CIRCLE_REPOSITORY_URL')
 
+if len(sys.argv) < 2:
+	print "Must provide Max Queue Time in *minutes* as script argument"
+	exit (1)
+max_time = int(sys.argv[1])
+print "This build will block until all previous builds complete."
+print "Max Queue Time: " + str(max_time) + " minutes."
+
+
 
 api = circleci.CircleCI(circleci_key,build_num,user_name,project_name,repo_url)
+wait_time = 0
+loop_time = 30
+while True:
 
-print "checking status"
+	oldest_running_build_num = api.oldest_running_build_num()
+	if build_num <= oldest_running_build_num :
+		print "Front of the line, WooHoo!, Build continuing"
+		break
+	else:
+		print "This build (" + str(build_num) + ") is queued, waiting for build number (" + str(oldest_running_build_num) + ") to complete."
+		print "Total Queue time: " + str(wait_time) + " seconds."
+	
+
+	if wait_time >= (max_time * 60):
+		print "Max wait time exceeded, cancelling this build."
+		api.cancel_current()
+		exit(1)
+
+	time.sleep(loop_time)
+	wait_time += loop_time
+
+
