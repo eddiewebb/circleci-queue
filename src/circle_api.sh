02720999cf941932b1f753b2ac6c6aabc9b86ebf
Eddie Webb <ollitech@gmail.com>
2018-04-14 14:08:20 -0400

    queue by specific job
    Uses the provided CIRCLE_JOB to block on previous builds only if the current job is active elsewhere.

--- src/circle_api.sh
+++ src/circle_api.sh
@@ -8,9 +8,15 @@ fi
 load_oldest_running_build_num(){
 	jobs_api_url_template="https://circleci.com/api/v1.1/project/${VCS_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}?circle-token=${CIRCLECI_API_KEY}&filter=running"
 	#jobs_api_url_template="http://localhost:5000"
-	
+
 	#negative index grabs last (oldest) job in returned results.
-	oldest=`curl -s $jobs_api_url_template | jq '.[-1].build_num'`
+	if [ -z ${CIRCLE_JOB} ];then
+		echo "No Job variable, blocking on any runnin jobs for this project."
+		oldest=`curl -s $jobs_api_url_template | jq '.[-1].build_num'`
+	else
+		echo "Only blocking for runnin jobs matching: ${CIRCLE_JOB}"
+		oldest=`curl -s $jobs_api_url_template | jq '. | map(select(.build_parameters.CIRCLE_JOB=="deploy-cf")) | .[-1].build_num'`
+	fi
 	if [ -z $oldest ];then
 		echo "API Call for existing jobs failed, failing this build.  Please check API token"
 		exit 1
@@ -28,5 +34,3 @@ cancel_current_build(){
 	cancel_api_url_template="https://circleci.com/api/v1.1/project/${VCS_TYPE}/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/${CIRCLE_BUILD_NUM}/cancel?circle-token=${CIRCLECI_API_KEY}"
 	curl -s -X POST $cancel_api_url_template > /dev/null
 }
-
-
